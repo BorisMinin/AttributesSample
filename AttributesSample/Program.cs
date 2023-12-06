0c38db03a5ac8d4e22ba9b9d4185ece7e8a5b058
BMinin <boris.minin@outlook.com>
2023-12-06 15:54:32 +0500

    Add project files.

---
+++ AttributesSample/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AttributesSample
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var students = new List<Student>
+            {
+                new Student { Name = "Ivan Ivanov1", Grades = 4, Email = "ivan@example.com" },
+                new Student { Name = "Alex Petrov", Grades = 3, Email = "alex@example.com" },
+                new Student { Name = "Marry Sidorova", Grades = 5, Email = "mariaexample.com" }
+            };
+
+            ValidateStudents(students);
+        }
+
+        public static void ValidateStudents(List<Student> students)
+        {
+            foreach (var student in students)
+            {
+                var validationResults = new List<ValidationResult>();
+                var validationContext = new ValidationContext(student);
+
+                bool isValid = Validator.TryValidateObject(student, validationContext, validationResults, true);
+
+                if (!isValid)
+                {
+                    Console.WriteLine($"Validation errors for student {student.Name}:");
+
+                    foreach (var validationResult in validationResults)
+                    {
+                        Console.WriteLine(validationResult.ErrorMessage);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ AttributesSample/Student.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AttributesSample
+{
+    public class Student
+    {
+        // the name must not contain numbers
+        [RegularExpression(@"^[A-Za-z.-]+(\s*[A-Za-z.-]+)*$", ErrorMessage = "The name must not contain a number")]
+        public string Name { get; set; }
+
+        // The score should be in the range of 2 to 5
+        [Range (2,5, ErrorMessage = "The score must be at least 2 and no more than 5")]
+        public int Grades { get; set; }
+
+        // the mail must be valid
+        [RegularExpression(@"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$", ErrorMessage = "The email is uncorrect")]
+        public string Email { get; set; }
+    }
+}
